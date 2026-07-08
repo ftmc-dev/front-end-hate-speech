6ef36d90f15609d49d89934ee0c55e9d1a57186b
hanga.lagoue <hanga.lagoue@lao-sarl.cm>
2026-07-08 17:47:03 +0100

    refactor(master): support typescript for env variables

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,13 +1,7 @@
-interface ViteTypeOptions {
-  // By adding this line, you can make the type of ImportMetaEnv strict
-  // to disallow unknown keys.
-  // strictImportMetaEnv: unknown
-}
-
 interface ImportMetaEnv {
-  readonly VITE_REST_BASE_URL: string
+  readonly VITE_REST_BASE_URL: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
